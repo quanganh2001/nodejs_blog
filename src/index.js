a297bf2e0cefb2cf183b34444a9ab43addb95720
quanganh2001 <cocoi5659@gmail.com>
2023-10-07 15:08:02 +0700

    Query string parameters

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', engine({
@@ -19,10 +19,15 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
 	res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
 	res.render('news');
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
+app.get('/search', (req, res) => {
+	console.log(req.query.q);
+	res.render('search');
+});
+
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`));
